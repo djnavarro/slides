adf7dbf0ae59be4012db7df57b4dbb1bccbc19b9
Danielle Navarro <d.navarro@unsw.edu.au>
2021-03-11 14:14:04 +1100

    customises theme

--- docs/project-structure/style/xaringan-themer.R
+++ docs/project-structure/style/xaringan-themer.R
@@ -4,9 +4,10 @@ library(xaringanthemer)
 
 # set colors -------------------------------------------------------------------
 style_duo_accent(
-  primary_color      = "#0F4C81", # pantone classic blue
-  secondary_color    = "#A2B8D4", # pantone cashmere blue
+  primary_color      = "#3D5B59", # teal green 
+  secondary_color    = "#1E2E2C", # dark teal green
   header_font_google = google_font("Raleway"),
+  link_color = choose_dark_or_light("#1E2E2C", white = "#FCB5AC"), # salmon links
   text_font_google   = google_font("Raleway", "300", "300i"),
   code_font_google   = google_font("Fira Code"),
   text_font_size     = "30px",
